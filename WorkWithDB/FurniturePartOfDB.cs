42a4e182de1aa4ede9c4aad964ec608abe880c8a
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-06 13:53:33 +0300

    Make class for work with db partial

---
+++ WorkWithDB/FurniturePartOfDB.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ProductsCore;
+
+namespace WorkWithDBCore
+{
+    public static partial class DB
+    {
+        private static List<Furniture> furnitureTable = new List<Furniture>();
+
+        public static void AddFurniture(Furniture furniture)
+        {
+            furnitureTable.Add(furniture);
+        }
+
+        public static void SortFurniture()
+        {
+            furnitureTable.Sort();
+        }
+
+        public static string[] PrintFurniture()
+        {
+            string[] furnitureInfo = new string[furnitureTable.Count];
+
+            for (int i = 0; i < furnitureTable.Count; i++)
+            {
+
+                furnitureInfo[i] = furnitureTable[i].ToString();
+            }
+
+            return furnitureInfo;
+        }
+
+        public static void DeleteFurniture(Furniture furniture)
+        {
+            furnitureTable.Remove(furniture);
+        }
+
+        public static void SerializeFurnitureDB()
+        {
+            foreach (Furniture furniture in furnitureTable)
+            {
+                furniture.Serialize();
+            }
+        }
+        public static void DeserializeFurnitureDB()
+        {
+            furnitureTable = Furniture.Deserialize();
+        }
+
+       
+    }
+}
---
+++ WorkWithDB/RawMaterialPartOfDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductsCore;
+
+namespace WorkWithDB
+{
+    public static partial class DB
+    {
+        private static List<RawMaterial> rawMaterialTable = new List<RawMaterial>();
+
+        public static void AddRawMaterial(RawMaterial rawMaterial)
+        {
+            rawMaterialTable.Add(rawMaterial);
+        }
+
+        public static void SortRawMaterial()
+        {
+            rawMaterialTable.Sort();
+        }
+
+        public static string[] PrintRawMaterial()
+        {
+            string[] rawMaterialInfo = new string[rawMaterialTable.Count];
+
+            for (int i = 0; i < rawMaterialTable.Count; i++)
+            {
+
+                rawMaterialInfo[i] = rawMaterialTable[i].ToString();
+            }
+
+            return rawMaterialInfo;
+        }
+
+        public static void DeleteRawMaterial(RawMaterial rawMaterial)
+        {
+            rawMaterialTable.Remove(rawMaterial);
+        }
+
+        public static void SerializeRawMaterialDB()
+        {
+            foreach (RawMaterial rawMaterial in rawMaterialTable)
+            {
+                rawMaterial.Serialize();
+            }
+        }
+
+        public static void DeserializeRawMaterialDB()
+        {
+            rawMaterialTable = RawMaterial.Deserialize();
+        }
+    }
+}
